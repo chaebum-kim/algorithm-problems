043aec5a1915e1dda598bec66525e9384ee19077
Chaebum Kim <tw2cha@gmail.com>
2021-02-18 11:40:57 +0900

    Modify SLinkedList

--- linkedlist/SLinkedList.py
+++ linkedlist/SLinkedList.py
@@ -18,12 +18,22 @@ class SLinkedList:
             self.tail.next = node
             self.tail = self.tail.next
 
+        return self.tail
+
     def generateList(self, length: int):
         for number in range(1, length+1):
             node = Node(key=number)
             self.append(node)
 
         return self
+
+    def getNode(self, key):
+        current = self.head
+        while current is not None:
+            if current.key == key:
+                return current
+            current = current.next
+        return None
 
     def listprint(self):
         current = self.head
