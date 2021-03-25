dad8f631d9eb7319ba9a2f46ccb45982383be0c0
Chaebum Kim <tw2cha@gmail.com>
2021-03-25 18:33:06 +0900

    Array question #2 modified

--- array/array02.py
+++ array/array02.py
@@ -1,79 +1,57 @@
 ''' Question:
 *   You are given an array of positive integers where each integer represents
 *   the height of a vertical line on a chart. Find two lines which together
-*   with x-axis forms a container that would hold the greates amount of water.
+*   with x-axis forms a container that would hold the greatest amount of water.
 *   Return the area of water it would hold.
 '''
 
 
 # Brute force solution
-def getMaxContainer(heights):
+def get_area_brute(heights: list) -> int:
 
-    maxArea = 0
-
-    # Loop through heights
-    length = len(heights)
-    for i in range(0, length):
-        for j in range(i + 1, length):
-            # Compute height of container
-            height = min([heights[i], heights[j]])
-
-            # Compute width of container
+    max_area = 0
+    for i, num1 in enumerate(heights):
+        for j, num2 in enumerate(heights[i+1:], i+1):
+            height = min(num1, num2)
             width = j - i
-
-            # Compute area of container
             area = height * width
+            max_area = max(max_area, area)
+    return max_area
 
-            # Compare area to maxArea
-            if area > maxArea:
-                maxArea = area
-
-    return maxArea
-
-# Time complexity: O(n^2)
+# Time complexity: O(N^2)
 # Space complexity: O(1)
 
 
-# Optimized solution
-def getMaxContainer2(heights):
+# Optimal solution
+def get_area_optimal(heights: list) -> int:
 
-    maxArea = 0
+    max_area = 0
+    pl = 0
+    pr = len(heights) - 1
 
-    # Set pointers on both ends of array
-    p1 = 0
-    p2 = len(heights) - 1
-
-    # Loop until pointers meet
-    while p1 < p2:
-        # Compute height
-        height = min([heights[p1], heights[p2]])
-
-        # Compute width
-        width = p2 - p1
-
-        # Compute area
+    while pl < pr:
+        height = min(heights[pl], heights[pr])
+        width = pr - pl
         area = height * width
-
-        # Compare area to maxArea
-        if area > maxArea:
-            maxArea = area
-
-        # Move pointer that points to smaller height towards center
-        if heights[p1] < heights[p2]:
-            p1 += 1
+        max_area = max(max_area, area)
+        if heights[pl] < heights[pr]:
+            pl += 1
         else:
-            p2 -= 1
+            pr -= 1
+    return max_area
 
-    return maxArea
-
-# Time complexity: O(n)
+# Time complexity: O(N)
 # Space complexity: O(1)
 
 
 # Test
-print(getMaxContainer([7, 1, 2, 3, 9]))
-print(getMaxContainer([7]))
-print(getMaxContainer([6, 9, 3, 4, 5, 8]))
-print(getMaxContainer2([7, 1, 2, 3, 9]))
-print(getMaxContainer2([7]))
-print(getMaxContainer2([6, 9, 3, 4, 5, 8]))
+if __name__ == '__main__':
+    heights1 = [1, 3, 6, 2, 5]
+    heights2 = [1]
+
+    # Expected result: 10, 0
+    print(get_area_brute(heights1))
+    print(get_area_brute(heights2))
+
+    print(get_area_optimal(heights1))
+    print(get_area_optimal(heights2))
