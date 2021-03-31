08bd8f418210eef6c10ae1f800e240b225e22506
Chaebum Kim <tw2cha@gmail.com>
2021-03-31 17:26:33 +0900

    String question #4

--- string/string04.py
+++ string/string04.py
@@ -2,106 +2,62 @@
 *   Given a string, determine if it is almost a palindrome.
 *   A string is almost a palindrome if it becomes a palindrome by removing 1 letter.
 *   Consider only alphanumeric characters and ignore case sensitivity
+*   https://leetcode.com/problems/valid-palindrome-ii/
 '''
 
 import re
-import unittest
 
 
-# My solution
-def isAlmostPalindrome1(string):
-    # Consider only alphanumeric characters
-    string = re.sub(r"[^a-zA-Z0-9]", "", string).lower()
+# Brute force solution
+def is_almost_palindrome_brute(s: str) -> bool:
 
-    # Set pointers
-    pL = 0
-    pR = len(string) - 1
+    s = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
+    pl, pr = 0, len(s)-1
 
-    # Loop through string
-    while pL < pR:
-
-        if string[pL] != string[pR]:
-            new = string[pL+1:pR+1]
-            reversedNew = new[::-1]
-
-            if new == reversedNew:
+    while pl < pr:
+        if s[pl] != s[pr]:
+            new = s[pl+1:pr+1]
+            reversed_new = new[::-1]
+            if new == reversed_new:
                 return True
 
-            new = string[pL:pR]
-            reversedNew = new[::-1]
-
-            if new == reversedNew:
+            new = s[pl:pr]
+            reversed_new = new[::-1]
+            if new == reversed_new:
                 return True
 
             return False
 
-        pL += 1
-        pR -= 1
+        pl += 1
+        pr -= 1
 
     return True
 
-# Time complexity: O(n)
-# Space complexity: O(n)
+# Time complexity: O(N)
+# Space complexity: O(N)
 
 
 # Optimal Solution
-def isValidSubPalindrome(string, pL, pR):
-    while pL < pR:
-        if string[pL] != string[pR]:
-            return False
+def is_almost_palindrome_optimal(s: str) -> bool:
+    def is_valid(s: str, pl: int, pr: int) -> bool:
+        while pl < pr:
+            if s[pl] != s[pr]:
+                return False
+            pl += 1
+            pr -= 1
+        return True
 
-        pL += 1
-        pR -= 1
+    s = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
+    pl, pr = 0, len(s) - 1
 
-    return True
-
-
-def isAlmostPalindrome2(string):
-
-    # Consider only alphanumeric characters
-    string = re.sub(r"[^a-zA-Z0-9]", "", string).lower()
-
-    # Set pointers
-    pL = 0
-    pR = len(string) - 1
-
-    # Loop through string
-    while pL < pR:
-
-        if string[pL] != string[pR]:
-            return isValidSubPalindrome(string, pL+1, pR) or isValidSubPalindrome(string, pL, pR-1)
-
-        pL += 1
-        pR -= 1
+    while pl < pr:
+        if s[pl] != s[pr]:
+            return self.is_valid(s, pl+1, pr) or self.is_valid(s, pl, pr-1)
+        pl += 1
+        pr -= 1
 
     return True
 
-# Time complexity: O(n)
+
+# Time complexity: O(N)
 # Space complexity: O(1)
-
-
-# Test
-class TestSolution(unittest.TestCase):
-    def test_isAlmostPalindrome1(self):
-        self.assertEqual(isAlmostPalindrome1("race a car"), True)
-        self.assertEqual(isAlmostPalindrome1("abccdba"), True)
-        self.assertEqual(isAlmostPalindrome1("abcdefdba"), False)
-        self.assertEqual(isAlmostPalindrome1(""), True)
-        self.assertEqual(isAlmostPalindrome1("a"), True)
-        self.assertEqual(isAlmostPalindrome1("ab"), True)
-        self.assertEqual(isAlmostPalindrome1(
-            "aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga"), True)
-
-    def test_isAlmostPalindrome2(self):
-        self.assertEqual(isAlmostPalindrome2("race a car"), True)
-        self.assertEqual(isAlmostPalindrome2("abccdba"), True)
-        self.assertEqual(isAlmostPalindrome2("abcdefdba"), False)
-        self.assertEqual(isAlmostPalindrome2(""), True)
-        self.assertEqual(isAlmostPalindrome2("a"), True)
-        self.assertEqual(isAlmostPalindrome2("ab"), True)
-        self.assertEqual(isAlmostPalindrome2(
-            "aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga"), True)
-
-
-if __name__ == '__main__':
-    unittest.main()
