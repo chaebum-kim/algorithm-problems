b017aa7f13e0448b810a609e1419bc6a20a71b8a
Chaebum Kim <tw2cha@gmail.com>
2021-04-09 17:51:16 +0900

    2D array question #2

--- 2d_array/2d_array02.py
+++ 2d_array/2d_array02.py
@@ -1,8 +1,11 @@
 ''' Question:
-*   Given a 2D array containing 0's(empty cell), 1's(fresh orange)) and 2's(rotten orange).
+*   Given a 2D array containing 0's(empty cell), 1's(fresh orange) and 2's(rotten orange).
 *   Every minute, all fresh orange immediately adjacent (4 directions) to rotten oranges will rot.
 *   How many minutes must pass until all oranges are rotten?
+*   https://leetcode.com/problems/rotting-oranges/
 '''
+
+from collections import deque
 
 
 directions = [
@@ -13,56 +16,42 @@ directions = [
 ]
 
 
-def minutes_to_rot(matrix: list) -> int:
+def minutes_to_rot(grid: List[List[int]]) -> int:
 
-    rotten_oranges = []
-    count_fresh_oranges = 0
-    mins = 0
+    m, n = len(grid), len(grid[0])
+    rotten_oranges = deque()
+    count_fresh = 0
+    for row in range(m):
+        for col in range(n):
+            if grid[row][col] == 1:
+                count_fresh += 1
+            elif grid[row][col] == 2:
+                rotten_oranges.append([row, col])
 
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            if matrix[i][j] == 2:
-                rotten_oranges.append([i, j])
-            elif matrix[i][j] == 1:
-                count_fresh_oranges += 1
-    if not rotten_oranges and not count_fresh_oranges:
+    if rotten_oranges and not count_fresh:
         return 0
 
-    count_rotten_oranges = len(rotten_oranges)
-
+    mins = 0
     while rotten_oranges:
-        if not count_rotten_oranges:
-            count_rotten_oranges = len(rotten_oranges)
+        if count_fresh:
             mins += 1
 
-        current = rotten_oranges.pop(0)
-        count_rotten_oranges -= 1
-        row = current[0]
-        col = current[1]
+        count_rotten = len(rotten_oranges)
+        for i in range(count_rotten):
+            current = rotten_oranges.popleft()
+            row = current[0]
+            col = current[1]
+            for direction in directions:
+                adj_row = row + direction[0]
+                adj_col = col + direction[1]
+                if 0 <= adj_row < m and 0 <= adj_col < n and grid[adj_row][adj_col] == 1:
+                    grid[adj_row][adj_col] = 2
+                    rotten_oranges.append([adj_row, adj_col])
+                    count_fresh -= 1
 
-        for direction in directions:
-            next_row = row + direction[0]
-            next_col = col + direction[1]
-            if next_row in range(len(matrix)) and next_col in range(len(matrix[0]))\
-                    and matrix[next_row][next_col] == 1:
-                matrix[next_row][next_col] = 2
-                count_fresh_oranges -= 1
-                rotten_oranges.append([next_row, next_col])
-
-    if count_fresh_oranges:
+    if count_fresh:
         return -1
     return mins
 
-# Time complexity: O(n)
-# Space complexity: O(n)
-
-
-# Test
-if __name__ == '__main__':
-    matrix1 = [[2, 1, 1, 0, 0], [1, 1, 1, 0, 0],
-               [0, 1, 1, 1, 1], [0, 1, 0, 0, 1]]
-    matrix2 = [[1, 1, 0, 0, 0], [2, 1, 0, 0, 0],
-               [0, 0, 0, 1, 2], [0, 1, 0, 0, 1]]
-
-    print(minutes_to_rot(matrix1))
-    print(minutes_to_rot(matrix2))
+# Time complexity: O(N)
+# Space complexity: O(N)
