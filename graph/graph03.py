3a3744f09e917a36f0d3c0900528ec396703aeb5
Chaebum Kim <tw2cha@gmail.com>
2021-04-15 23:43:02 +0900

    Graph question #3 / Dijkstra, Bellman-Ford

--- graph/graph03.py
+++ graph/graph03.py
@@ -4,44 +4,36 @@
 *   v is the target node, and w is the time taken to travel from the souce node
 *   to the target node. Send a signal from node k, return how long it takes
 *   for all nodes to receive the signal. Retrun -1 if it's impossible.
+*   https://leetcode.com/problems/network-delay-time/
 '''
 
 import math
-import importlib.util
-spec = importlib.util.spec_from_file_location(
-    "priority_queue", "/home/chaebum_kim/projects/problems/queue/priority_queue.py")
-priority_queue = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(priority_queue)
+from queue import PriorityQueue
 
 
-# Dijkstra's algorithm
-def newtwork_delay_time1(n: int, k: int, times: list) -> int:
+# Dijkstra's algorithm(for directed, weighted graph)
+def newtwork_delay_time_dijkstra(n: int, k: int, times: list) -> int:
 
     # Build adjacency list from times
     adj_list = [[] for x in range(n)]
-    for time in times:
-        source = time[0] - 1
-        target = time[1] - 1
-        weight = time[2]
-        adj_list[source].append([target, weight])
+    for u, v, w in times:
+        adj_list[u-1].append([v-1, w])
 
-    # Initialize distances
+    # Initiailize distances
     distances = [math.inf for x in range(n)]
     distances[k-1] = 0
 
-    # Initialize min_heap
-    min_heap = priority_queue.PriorityQueue(
-        comparator=lambda a, b: distances[a] < distances[b])
-    min_heap.push(k-1)
+    # Initialize min heap
+    pq = PriorityQueue()
+    pq.put((distances[k-1], k-1))
 
-    while not min_heap.is_empty():
-        current = min_heap.pop()
-        for connection in adj_list[current]:
-            target = connection[0]
-            weight = connection[1]
-            if distances[current] + weight < distances[target]:
-                distances[target] = distances[current] + weight
-                min_heap.push(target)
+    while not pq.empty():
+        current = pq.get()[1]
+        for target, weight in adj_list[current]:
+            new_distance = distances[current] + weight
+            if new_distance < distances[target]:
+                distances[target] = new_distance
+                pq.put((distances[target], target))
 
     result = max(distances)
     return -1 if result == math.inf else result
@@ -51,24 +43,21 @@ def newtwork_delay_time1(n: int, k: int, times: list) -> int:
 
 
 # Bellman-Ford Algorithm (When weight can be negative, no negative cycle)
-def newtwork_delay_time2(n: int, k: int, times: list) -> int:
+def newtwork_delay_time_bellmanford(n: int, k: int, times: list) -> int:
 
     # Initialize distances
     distances = [math.inf for x in range(n)]
     distances[k-1] = 0
 
-    # Calculate opitmal distances
     for i in range(n-1):
         count = 0
-        for time in times:
-            source = time[0] - 1
-            target = time[1] - 1
-            weight = time[2]
-            if distances[source] + weight < distances[target]:
-                distances[target] = distances[source] + weight
+        for source, target, weight in times:
+            new_distance = distances[source-1] + weight
+            if new_distance < distances[target-1]:
+                distances[target-1] = new_distance
                 count += 1
 
-        if count == 0:
+        if not count:
             break
 
     result = max(distances)
@@ -76,27 +65,3 @@ def newtwork_delay_time2(n: int, k: int, times: list) -> int:
 
 # Time complexity: O(NE)
 # Space complexity: O(N)
-
-
-# Test
-if __name__ == '__main__':
-    n1 = 5
-    k1 = 1
-    times1 = [[1, 2, 9], [1, 4, 2], [2, 5, 1], [4, 2, 4],
-              [4, 5, 6], [3, 2, 3], [5, 3, 7], [3, 1, 5]]
-
-    n2 = 3
-    k2 = 2
-    times2 = [[2, 3, 4]]
-
-    n3 = 3
-    k3 = 1
-    times3 = [[1, 2, 8], [3, 1, 3]]
-
-    print(newtwork_delay_time1(n1, k1, times1))
-    print(newtwork_delay_time1(n2, k2, times2))
-    print(newtwork_delay_time1(n3, k3, times3))
-
-    print(newtwork_delay_time2(n1, k1, times1))
-    print(newtwork_delay_time2(n2, k2, times2))
-    print(newtwork_delay_time2(n3, k3, times3))
