490b2ade1c6cdba9348999258c71e827aa02e391
Chaebum Kim <tw2cha@gmail.com>
2021-04-16 18:28:29 +0900

    Dynamic programmin #1

--- dynamic_programming/dynamic_programming01.py
+++ dynamic_programming/dynamic_programming01.py
@@ -3,105 +3,64 @@
 *   indicated by a cost array. Once you pay the cost for a step,
 *   You can either climb one or two steps. Find the minimum cost
 *   to reach the top of the staircase. Your first step can either be the first or second step.
+*   https://leetcode.com/problems/min-cost-climbing-stairs/
 '''
 
 
 # Recursive solution
-def find_minimum_cost1(cost: list) -> int:
+def min_cost_climbing_stairs1(cost: list) -> int:
+    def min_cost_so_far(n):
+        if n < 0:
+            return 0
+        if 0 <= n <= 1:
+            return cost[n]
+        return cost[n] + min(min_cost_so_far(n-1), min_cost_so_far(n-2))
 
-    n = len(cost)
-    return min([min_cost(cost, n-1), min_cost(cost, n-2)])
+    last = len(cost)
+    return min(min_cost_so_far(last-1), min_cost_so_far(last-2))
 
-
-def min_cost(cost: list, i: int) -> int:
-
-    if i < 0:
-        return 0
-    elif i == 0 or i == 1:
-        return cost[i]
-
-    return cost[i] + min([min_cost(cost, i-1), min_cost(cost, i-2)])
-
-# Time complexity: O(2^n)
+# Time complexity: O(2^N)
 # Space complexity: O(N)
 
 
 # Memoizing redundant recursive calls
-def find_minimum_cost2(cost: list) -> int:
+def min_cost_climbing_stairs2(cost: list) -> int:
+    def min_cost_so_far(n, dp):
+        if n < 0:
+            return 0
+        if 0 <= n <= 1:
+            return cost[n]
 
-    n = len(cost)
-    calculated = [None for x in range(n)]
-    return min([min_cost2(cost, n-1, calculated),
-                min_cost2(cost, n-2, calculated)])
+        if dp.get(n) is not None:
+            return dp.get(n)
 
+        result = cost[n] + min(min_cost_so_far(n-1, dp),
+                               min_cost_so_far(n-2, dp))
+        dp[n] = result
+        return result
 
-def min_cost2(cost: list, i: int, calculated: list) -> int:
-
-    if i < 0:
-        return 0
-    elif i == 0 or i == 1:
-        return cost[i]
-
-    if calculated[i] is not None:
-        return calculated[i]
-
-    calculated[i] = cost[i] + \
-        min([min_cost2(cost, i-1, calculated), min_cost2(cost, i-2, calculated)])
-    return calculated[i]
+    last = len(cost)
+    dp = {}
+    return min(min_cost_so_far(last-1, dp), min_cost_so_far(last-2, dp))
 
 # Time complexity: O(N)
 # Space complexity: O(N)
 
 
 # Bottom-up approach
-def find_minimum_cost3(cost: int) -> int:
+def min_cost_climbing_stairs3(cost: int) -> int:
 
-    n = len(cost)
-    if n == 0:
+    length = len(cost)
+    if length <= 1:
         return 0
-    if n == 1:
-        return cost[1]
 
-    calculated = [None for x in range(n)]
-    for i, value in enumerate(cost):
-        if i < 2:
-            calculated[i] = value
-        else:
-            calculated[i] = value + min([calculated[i-1], calculated[i-2]])
-    return min([calculated[n-1], calculated[n-2]])
+    prev_prev, prev = cost[0], cost[1]
+    for i in range(2, length):
+        min_cost_so_far = min(prev_prev, prev) + cost[i]
+        prev_prev = prev
+        prev = min_cost_so_far
 
-# Time complexity: O(N)
-# Space complexity: O(N)
-
-
-# Bottom-up optimization
-def find_minimum_cost4(cost: int) -> int:
-
-    n = len(cost)
-    if n == 0:
-        return 0
-    if n == 1:
-        return cost[1]
-
-    two_step_before = cost[0]
-    one_step_before = cost[1]
-
-    for i in range(2, n):
-        current = cost[i] + min([one_step_before, two_step_before])
-        two_step_before = one_step_before
-        one_step_before = current
-
-    return min([one_step_before, two_step_before])
+    return min(prev_prev, prev)
 
 # Time complexity: O(N)
 # Space complexity: O(1)
-
-
-# Test
-if __name__ == '__main__':
-    cost = [20, 15, 30, 5]
-
-    print(find_minimum_cost1(cost))
-    print(find_minimum_cost2(cost))
-    print(find_minimum_cost3(cost))
-    print(find_minimum_cost4(cost))
